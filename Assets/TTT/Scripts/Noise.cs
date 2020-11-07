9eeb25778cf6fc8d30bb7873461965cbef730a43
powenyao <powenyao@usc.edu>
2020-11-07 15:18:36 -0800

    Folder Structure Basic Perlin Map for Terrain

---
+++ Assets/TTT/Scripts/Noise.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+    //Basic Perlin Noise
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+    {
+        var noiseMap = new float [mapWidth, mapHeight];
+
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                var sampleX = x / scale;
+                var sampleY = y / scale;
+                var perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+
+        return noiseMap;
+    }
+
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves,
+        float persistance, float lacunarity, Vector2 offset)
+    {
+        var noiseMap = new float [mapWidth, mapHeight];
+
+        var prng = new System.Random(seed);
+        var octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++)
+        {
+            var offsetX = prng.Next(-100000, 10000) + offset.x;
+            var offsetY = prng.Next(-10000, 10000) + offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
+
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+
+        var maxNoiseHeight = float.MinValue;
+        var minNoiseHeight = float.MaxValue;
+
+        float halfWidth = mapWidth / 2f;
+        float halfHeight = mapHeight / 2f;
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for (int i = 0; i < octaves; i++)
+                {
+                    var sampleX = (x-halfWidth) / scale * frequency + octaveOffsets[i].x;
+                    var sampleY = (y-halfHeight) / scale * frequency + octaveOffsets[i].y;
+
+                    var perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1; //between -1 to 1
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistance; //decreases each octave
+                    frequency *= lacunarity; //increases each octave
+                }
+
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+
+                if (noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            }
+        }
+
+        return noiseMap;
+    }
+}
---
+++ Assets/TTT/Scripts/Terrain/MapDisplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MapDisplay : MonoBehaviour
+{
+    public Renderer textureRender;
+
+    public void DrawNoiseMap(float[,] noiseMap)
+    {
+        var width = noiseMap.GetLength(0);
+        var height = noiseMap.GetLength(1);
+
+        var texture = new Texture2D(width, height);
+        
+        var colorMap = new Color[width * height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+            }
+        }
+        texture.SetPixels(colorMap);
+        texture.Apply();
+
+        textureRender.sharedMaterial.mainTexture = texture;
+        textureRender.transform.localScale = new Vector3(width, 1, height);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
