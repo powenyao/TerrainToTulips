e146057de41e7c2019b77abbfe1fcd88bd3e3072
powenyao <powenyao@usc.edu>
2020-11-16 16:50:18 -0800

    Added Cinemachine for moving cameras Adding animation for moving the camera dolly in planet scene simple transition to the city scene Added StarsSkyboxShader for background for now Moved files around for organization

---
+++ Assets/GetPlanetRadius.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetPlanetRadius : MonoBehaviour
+{
+    public Planet myPlanet;
+
+    private Vector3 startingPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(0, myPlanet.shapeSettings.planetRadius-transform.position.y, 0);
+    }
+}
---
+++ Assets/TTT/Scripts/Planet/EffectManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.SceneManagement;
+
+public class EffectManager : MonoBehaviour
+{
+    public Planet Earth;
+    public ShapeSettings Shape_Earth;
+    public ColorSettings Color_Earth;
+    public ShapeSettings Shape_Default;
+    public ColorSettings Color_Default;
+    public ShapeSettings Shape_EarthCurrent;
+    public ColorSettings Color_EarthCurrent;
+
+    public CinemachineVirtualCamera vc;
+    
+    private float timeElapsed = 0;
+    public float lerpDuration = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shape_EarthCurrent.CopyShapeSettings(Shape_Default);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //vc.bod
+        if (timeElapsed < lerpDuration)
+        {
+            var t = timeElapsed / lerpDuration;
+            Shape_EarthCurrent.planetRadius = Mathf.Lerp(Shape_Default.planetRadius, Shape_Earth.planetRadius, t);
+
+
+            for (int i = 0; i < Shape_EarthCurrent.noiseLayers.Length; i++)
+            {
+                if (i == 0)
+                {
+                    Shape_EarthCurrent.noiseLayers[i].noiseSettings.simpleNoiseSettings.strength = Mathf.Lerp(
+                        Shape_Default.noiseLayers[i].noiseSettings.simpleNoiseSettings.strength,
+                        Shape_Earth.noiseLayers[i].noiseSettings.simpleNoiseSettings.strength, t);
+                    Shape_EarthCurrent.noiseLayers[i].noiseSettings.simpleNoiseSettings.baseRoughness = Mathf.Lerp(
+                        Shape_Default.noiseLayers[i].noiseSettings.simpleNoiseSettings.baseRoughness,
+                        Shape_Earth.noiseLayers[i].noiseSettings.simpleNoiseSettings.baseRoughness, t);
+                    Shape_EarthCurrent.noiseLayers[i].noiseSettings.simpleNoiseSettings.roughness = Mathf.Lerp(
+                        Shape_Default.noiseLayers[i].noiseSettings.simpleNoiseSettings.roughness,
+                        Shape_Earth.noiseLayers[i].noiseSettings.simpleNoiseSettings.roughness, t);
+                    Shape_EarthCurrent.noiseLayers[i].noiseSettings.simpleNoiseSettings.persistence = Mathf.Lerp(
+                        Shape_Default.noiseLayers[i].noiseSettings.simpleNoiseSettings.persistence,
+                        Shape_Earth.noiseLayers[i].noiseSettings.simpleNoiseSettings.persistence, t);
+                    Shape_EarthCurrent.noiseLayers[i].noiseSettings.simpleNoiseSettings.minValue = Mathf.Lerp(
+                        Shape_Default.noiseLayers[i].noiseSettings.simpleNoiseSettings.minValue,
+                        Shape_Earth.noiseLayers[i].noiseSettings.simpleNoiseSettings.minValue, t);
+                }
+                else
+                {
+                    Shape_EarthCurrent.noiseLayers[i].noiseSettings.rigidNoiseSettings.strength = Mathf.Lerp(
+                        Shape_Default.noiseLayers[i].noiseSettings.rigidNoiseSettings.strength,
+                        Shape_Earth.noiseLayers[i].noiseSettings.rigidNoiseSettings.strength, t);
+                    Shape_EarthCurrent.noiseLayers[i].noiseSettings.rigidNoiseSettings.baseRoughness = Mathf.Lerp(
+                        Shape_Default.noiseLayers[i].noiseSettings.rigidNoiseSettings.baseRoughness,
+                        Shape_Earth.noiseLayers[i].noiseSettings.rigidNoiseSettings.baseRoughness, t);
+                    Shape_EarthCurrent.noiseLayers[i].noiseSettings.rigidNoiseSettings.roughness = Mathf.Lerp(
+                        Shape_Default.noiseLayers[i].noiseSettings.rigidNoiseSettings.roughness,
+                        Shape_Earth.noiseLayers[i].noiseSettings.rigidNoiseSettings.roughness, t);
+                    Shape_EarthCurrent.noiseLayers[i].noiseSettings.rigidNoiseSettings.persistence = Mathf.Lerp(
+                        Shape_Default.noiseLayers[i].noiseSettings.rigidNoiseSettings.persistence,
+                        Shape_Earth.noiseLayers[i].noiseSettings.rigidNoiseSettings.persistence, t);
+                    Shape_EarthCurrent.noiseLayers[i].noiseSettings.rigidNoiseSettings.minValue = Mathf.Lerp(
+                        Shape_Default.noiseLayers[i].noiseSettings.rigidNoiseSettings.minValue,
+                        Shape_Earth.noiseLayers[i].noiseSettings.rigidNoiseSettings.minValue, t);
+                    Shape_EarthCurrent.noiseLayers[i].noiseSettings.rigidNoiseSettings.weightMultiplier = Mathf.Lerp(
+                        Shape_Default.noiseLayers[i].noiseSettings.rigidNoiseSettings.weightMultiplier,
+                        Shape_Earth.noiseLayers[i].noiseSettings.rigidNoiseSettings.weightMultiplier, t);
+                }
+            }
+            Earth.OnShapeSettingsUpdated();
+            
+            timeElapsed += Time.deltaTime;
+        }
+        else
+        {
+            Shape_EarthCurrent.planetRadius = Shape_Earth.planetRadius;
+
+            SceneManager.LoadScene("TestScene_City");
+        }
+    }
+    
+}
